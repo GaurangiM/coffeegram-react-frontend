ae725e16f98b81dacbac693e98b49b096afecfff
GaurangiM <manegaurangi89@gmail.com>
2021-07-13 11:42:31 +0200

    Add heroku db link

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl = process.env.API_URL || "https://coffeegram-server.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
